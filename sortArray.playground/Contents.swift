888ed2e6d1077ba2d46563436819b7129fd8edd4
Hunter McNeil <huntamaneil@gmail.com>
2020-10-03 22:40:44 -0600

    sortArray playground fun

---
+++ sortArray.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+func alternatingSort(a: [Int]) -> Bool {
+    var b: [Int] = []
+    var a = a
+    
+    while a.count > 0 {
+        b.append(a.removeFirst())
+        b.append(a.removeLast())
+        print(b)
+    }
+    return true
+}
+
+alternatingSort(a: [1,2,3,4,5,6,7])
