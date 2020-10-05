e0877275829b83ab8c716098c71a585175471884
Hunter McNeil <huntamaneil@gmail.com>
2020-10-05 15:14:17 -0600

    creating and reversing a linked list

---
+++ reversedLinkedList.playground/Contents.swift
@@ -0,0 +1,47 @@
+import UIKit
+
+// If needed, create a linked list first
+class Node {
+    let value: Int
+    var next: Node?
+    
+    init(value:Int, next: Node?) {
+        self.value = value
+        self.next = next
+    }
+}
+
+let threeNode = Node(value: 3, next: nil)
+let twoNode = Node(value: 2, next: threeNode)
+let oneNode = Node(value: 1, next: twoNode)
+
+func printList(head: Node?) {
+    print("Printing out list of nodes")
+    var currentNode = head
+    while currentNode != nil {
+        print(currentNode?.value ?? -1)
+        currentNode = currentNode?.next
+    }
+}
+
+printList(head: oneNode)
+
+//Reverse the linked list
+func reverseList(head: Node?) -> Node? {
+    
+    var currentNode = head
+    var prev: Node?
+    var next: Node?
+    
+    while currentNode != nil {
+        next = currentNode?.next
+        currentNode?.next = prev
+        prev = currentNode
+        currentNode = next
+    }
+    return prev
+}
+
+let myReversedList = reverseList(head: oneNode)
+
+printList(head: myReversedList)
